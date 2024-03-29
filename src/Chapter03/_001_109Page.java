53b4bf7da0d360c398a378ee37c8e231dd7b2267
dongso <ehdth5806@naver.com>
2024-03-29 23:15:14 +0900

    자바의 자료구조 2번째 수업 Commit.

---
+++ src/Chapter03/_001_109Page.java
@@ -0,0 +1,25 @@
+package Chapter03;
+
+public class _001_109Page {
+
+	public static void main(String[] args) {
+//		int i_arr2[][] = new int[2][3];
+		
+		// int i_arr2[][] = { {(0, 1, 2}, {10, 11, 12}}; ==> 배열선언과 초기화 동시에 할수 있다.
+		int i_arr2[][] = { {0, 1, 2}, {10, 11, 12} };
+		
+//		i_arr2[0][0] = 0;
+//		i_arr2[0][1] = 1;
+//		i_arr2[0][2] = 2;
+//		i_arr2[1][0] = 10;
+//		i_arr2[1][1] = 11;
+//		i_arr2[1][2] = 12;
+		
+		System.out.println("i_arr2[0][0] = " + i_arr2[0][0]);
+		System.out.println("i_arr2[0][1] = " + i_arr2[0][1]);
+		System.out.println("i_arr2[0][2] = " + i_arr2[0][2]);
+		System.out.println("i_arr2[1][0] = " + i_arr2[1][0]);
+		System.out.println("i_arr2[1][1] = " + i_arr2[1][1]);
+		System.out.println("i_arr2[1][2] = " + i_arr2[1][2]);
+	}
+}
