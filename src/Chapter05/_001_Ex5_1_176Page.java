f8d11e02592c823be2932c382250193dcc7c0aa8
dongso <ehdth5806@naver.com>
2024-04-06 19:26:49 +0900

    자바의자류구조 10번째 수업 Commit.

---
+++ src/Chapter05/_001_Ex5_1_176Page.java
@@ -0,0 +1,12 @@
+package Chapter05;
+
+public class _001_Ex5_1_176Page {
+	
+	public static void main(String[] args) {
+		int sale[] = new int[]{157, 209, 251, 312};
+		
+		for(int i = 0; i < 4; i++) {
+			System.out.printf("%d/4ºÐ±â : sale[%d]= %d %n", i + 1, i, sale[i]);
+		}
+	}
+}
