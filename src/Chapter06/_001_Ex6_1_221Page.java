e64eb9512141a5f9501f956c3ec67c71d7cd9636
dongso <ehdth5806@naver.com>
2024-04-26 17:16:13 +0900

    자바의자료구조 24번째 수업 Commit.

--- src/Chapter06/_001_Ex6_1_221Page.java
+++ src/Chapter06/_001_Ex6_1_221Page.java
@@ -6,3 +6,35 @@ public class _001_Ex6_1_221Page {
 		
 	}
 }
+
+class LinkedList {
+	
+}
+
+class ListNode {
+	private String data;
+	public ListNode link;
+	
+	// 기본 생성자
+	public ListNode() { 
+		this.data = null;
+		this.link = null;
+	}
+	
+	// data필드 입력받는 생성자
+	public ListNode(String data) {
+		this.data = data;
+		this.link = null;
+	}
+	
+	// data필드, link노드를 입력받는 생성자
+	public ListNode(String data, ListNode link) {
+		this.data = data;
+		this.link = link;
+	}
+	
+	// ListNode객체의 data필드를 반환하는 함수
+	public String getData() {
+		return this.data;
+	}
+}
