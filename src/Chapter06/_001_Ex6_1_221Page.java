7e2e93c698baa95ac5099e3820bbcf6e9bb83c74
dongso <ehdth5806@naver.com>
2024-04-27 22:37:41 +0900

    자바의자료구조 25번째 수업 Commit.

--- src/Chapter06/_001_Ex6_1_221Page.java
+++ src/Chapter06/_001_Ex6_1_221Page.java
@@ -7,10 +7,35 @@ public class _001_Ex6_1_221Page {
 	}
 }
 
+// 1
 class LinkedList {
+	private ListNode head; // 노드 객체
+	
+	public LinkedList() { // 생성자
+		head = null; // 기본생성자 null로 초기화
+	}
+	
+	// 중간에 삽입
+	public void insertMiddleNode(ListNode pre, String data) {
+		ListNode newNode = new ListNode(data); // 새로 삽입될 노드의 데이터
+		
+		// 삽입 될 노드의 링크 필드에 pre(삽입할 위치의 앞에 있는 선행자노드 참조번지를)를 저장.
+		newNode.link = pre.link; 
+		
+		// 삽입될 선행자 노드의 링크필드에는 삽입 될 노드의 참조번지를 저장한다.
+		pre.link = newNode;
+	}
+	
+	// 마지막 노드에 삽입
+	public void insertLastNode() {
+		
+	}
+	
+	
 	
 }
 
+// 2
 class ListNode {
 	private String data;
 	public ListNode link;
