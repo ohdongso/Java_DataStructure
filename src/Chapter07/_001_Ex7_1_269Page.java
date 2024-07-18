f6ad90bddd3ea802b42374c1c695c3f98b352b39
dongso <ehdth5806@naver.com>
2024-07-19 00:04:02 +0900

    자바의자료구조 74번째 Commit.

--- src/Chapter07/_001_Ex7_1_269Page.java
+++ src/Chapter07/_001_Ex7_1_269Page.java
@@ -3,10 +3,6 @@ package Chapter07;
 public class _001_Ex7_1_269Page {
 	
 	public static void main(String[] args) {
-		int sale[] = new int[]{157, 209, 251, 312};
 		
-		for(int i = 0; i < 4; i++) {
-			System.out.printf("%d/4ºÐ±â : sale[%d]= %d %n", i + 1, i, sale[i]);
-		}
 	}
 }
