83f5930f0ef923c8ff46066e0331d60a4a9db2a7
dongso <ehdth5806@naver.com>
2024-07-20 16:23:34 +0900

    자바의자료구조 75번째 Commit.

--- src/Chapter07/_001_Ex7_1_269Page.java
+++ src/Chapter07/_001_Ex7_1_269Page.java
@@ -1,5 +1,56 @@
 package Chapter07;
 
+interface Stack {
+	boolean isEmpty();
+	void push(char item);
+	char pop();
+	void delete();
+	char peek();
+}
+
+class ArrayStack implements Stack {
+	private int top;
+	private int stackSize;
+	private char itemArray[];
+	
+	public ArrayStack(int stackSize) { // 생성자
+		top = -1; // stack의 index의 시작 값은 -1이다.
+		this.stackSize = stackSize; // 스택 크기
+		itemArray = new char[this.stackSize]; // 스택 배열 생성
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void push(char item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public char pop() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public char peek() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+} // ArrayStack 끝.
+
 public class _001_Ex7_1_269Page {
 	
 	public static void main(String[] args) {
