3f2b064c8a0ba31b2b7b728269133538877cee06
dongso <ehdth5806@naver.com>
2024-07-23 22:41:22 +0900

    자바의자료구조 76번째 Commit.

--- src/Chapter07/_001_Ex7_1_269Page.java
+++ src/Chapter07/_001_Ex7_1_269Page.java
@@ -21,32 +21,54 @@ class ArrayStack implements Stack {
 	
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return (top == -1);
 	}
-
+	
+	public boolean isFull() {
+		return (top == this.stackSize - 1);
+	}
+	
 	@Override
 	public void push(char item) {
-		// TODO Auto-generated method stub
-		
+		if(isFull()) {
+			System.out.println("Inserting fail! Array Stack is full!!");
+		} else {
+			itemArray[++top] = item;
+			System.out.println("Inserted Item : " + item);
+		}
 	}
 
 	@Override
 	public char pop() {
-		// TODO Auto-generated method stub
-		return 0;
+		if(isEmpty()) {
+			System.out.println("Deleting fail! Array Stack is empty!!");
+			return 0;
+		} else {
+			return itemArray[top--];
+		}
 	}
 
 	@Override
 	public void delete() {
-		// TODO Auto-generated method stub
-		
+		if(isEmpty()) {
+			System.out.println("Deleting fail! Array Stack is empty!!");
+		} else {
+			top--;
+		}
 	}
 
 	@Override
 	public char peek() {
-		// TODO Auto-generated method stub
-		return 0;
+		if(isEmpty()) {
+			System.out.println("Peeking fail! Array Stack is empty!!");
+			return 0;
+		} else {
+			return itemArray[top];
+		}
+	}
+	
+	public void printStack() {
+		
 	}
 	
 } // ArrayStack ³¡.
