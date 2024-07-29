390c0b0a884c472f8dd6b3a1e19a842777d85913
dongso <ehdth5806@naver.com>
2024-07-29 22:06:39 +0900

    자바의자료구조 81번쨰 Commit.

--- src/Chapter07/_001_Ex7_2_277Page.java
+++ src/Chapter07/_001_Ex7_2_277Page.java
@@ -1,7 +1,46 @@
 package Chapter07;
 
-public class _001_Ex7_2_277Page {
+interface Stack1 {
+	boolean isEmpty();
+	void push(char item);
+	char pop();
+	void delete();
+	char peek();
+}
+
+class StackNode {
+	char data;
+	StackNode link;
+}
+
+class LinkeStack implements Stack1 {
+	private StackNode top;
 	
+	@Override
+	public boolean isEmpty() {	
+		return false;
+	}
+
+	@Override
+	public void push(char item) {
+	}
+
+	@Override
+	public char pop() {	
+		return 0;
+	}
+
+	@Override
+	public void delete() {
+	}
+
+	@Override
+	public char peek() {
+		return 0;
+	}
+}
+
+public class _001_Ex7_2_277Page {
 	public static void main(String[] args) {
 		
 	}
