f82db07ba212e7e86e40e3f7621663dd4c1c93c7
dongso <ehdth5806@naver.com>
2024-07-31 22:08:52 +0900

    자바의자료구조 82번째 Commit.

--- src/Chapter07/_001_Ex7_2_277Page.java
+++ src/Chapter07/_001_Ex7_2_277Page.java
@@ -18,30 +18,69 @@ class LinkeStack implements Stack1 {
 	
 	@Override
 	public boolean isEmpty() {	
-		return false;
+		return (top == null);
 	}
 
 	@Override
 	public void push(char item) {
+		StackNode newNode = new StackNode();
+		newNode.data = item;
+		newNode.link = top;
+		top = newNode;
+		System.out.println("Inserted Item : " + item);
 	}
-
+	
 	@Override
-	public char pop() {	
-		return 0;
+	public char pop() {
+		if(isEmpty()) {
+			System.out.println("Deleting Fail! Linked Stack is empty!!");
+			return 0;
+		} else {
+			char item = top.data;
+			top = top.link;
+			return item;
+		}
 	}
 
 	@Override
 	public void delete() {
+		if(isEmpty()) {
+			System.out.println("Deleting fail! Linked Stack is empty!!");
+		} else {
+			top = top.link;
+		}
 	}
 
 	@Override
 	public char peek() {
-		return 0;
+		if(isEmpty()) {
+			System.out.println("Peeking fail! Linked Stack is empty!!");
+			return 0;
+		} else {
+			return top.data;
+		}
+	}
+	
+	public void printStack() {
+		if(isEmpty()) {
+			System.out.printf("Linked Stack is empty!! %n %n");
+		} else {
+			StackNode temp = top;
+			System.out.println("Linked Stack>> ");
+			while(temp != null) {
+				System.out.printf("\t %c \n", temp.data);
+				temp = temp.link;
+			} // while문 끝.
+			System.out.println();
+		}
 	}
 }
 
 public class _001_Ex7_2_277Page {
 	public static void main(String[] args) {
+		char deletedItem;
+		// ==> 여기서 부터
+		
 		
 	}
 }
