f86fcd233675d136e3478e7d94d2f6872f3244cd
dongso <ehdth5806@naver.com>
2024-08-17 23:36:24 +0900

    자바의자료구조 94번째 Commit.

--- src/Chapter07/_003_Ex7_3_285Page.java
+++ src/Chapter07/_003_Ex7_3_285Page.java
@@ -8,48 +8,76 @@ interface Stack2 {
 	char peek();
 }
 
-class StackNode1 {
+class stacknode1 {
 	char data;
-	StackNode1 link;
+	stacknode1 link;
 }
 
 class LinkedStack1 implements Stack2 {
-
+	private stacknode1 top;
+	
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return (top == null);
 	}
 
 	@Override
 	public void push(char item) {
-		// TODO Auto-generated method stub
-		
+		stacknode1 newNode = new stacknode1();
 	}
 
 	@Override
 	public char pop() {
-		// TODO Auto-generated method stub
-		return 0;
+		if(isEmpty()) {
+			System.out.println("Deleting fail! Linked Stack is empty!!");
+			return 0;
+		} else {
+			char item = top.data;
+			top = top.link;
+			return item;
+		}
 	}
 
 	@Override
 	public void delete() {
-		// TODO Auto-generated method stub
-		
+		if(isEmpty()) {
+			System.out.println("Deleting fail! Linked Stack is empty!!");
+		} else {
+			top = top.link;
+		}
 	}
 
 	@Override
 	public char peek() {
-		// TODO Auto-generated method stub
-		return 0;
+		if(isEmpty()) {
+			System.out.println("Peeking fail! Linked Stack is empty!!");
+			return 0;
+		} else {
+			return top.data;
+		}
 	}
+	
+	public void printStack() {
+		if(isEmpty()) {
+			System.out.printf("Linked Stack is empty!! %n %n");
+		} else {
+			stacknode1 temp = top;
+			System.out.println("Linked Stack>> ");
+			while(temp != null) {
+				System.out.printf("\t %c \n", temp.data);
+				temp = temp.link;
+			} // while문 끝.
+			System.out.println();
+		}
+	} // printStack() 메서드 끝.
 }
 
 class OptExp {
 	private String exp;
 	private int expSize;
 	private char testCh, openPair;
+	
+	// ==> 여기서 부터
 }
 
 public class _003_Ex7_3_285Page {
