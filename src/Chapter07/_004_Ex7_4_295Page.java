d00909b076f456bec9f5d19e204fc870f35d709b
dongso <ehdth5806@naver.com>
2024-09-05 23:37:21 +0900

    자바의정석 134번째 Commit.

--- src/Chapter07/_004_Ex7_4_295Page.java
+++ src/Chapter07/_004_Ex7_4_295Page.java
@@ -31,6 +31,34 @@ class LinkedStack2 {
 	}
 }
 
+class OptExp2 {
+	private String exp;
+	
+	public int evalPostfix(String postfix) {
+		LinkedStack2 S = new LinkedStack2();
+		exp = postfix;
+		int opr1, opr2, value;
+		char testCh;
+		for(int i = 0; i < 7; i++) {
+			testCh = exp.charAt(i);
+			if(testCh != '+' && testCh != '-' && testCh != '*' && testCh != '/') {
+				value = testCh - '0';
+				S.push(value);
+			} else {
+				opr2 = S.pop();
+				opr1 = S.pop();
+				switch(testCh) {
+					case '+' : S.push(opr1 + opr2); break;
+					case '-' : S.push(opr1 - opr2); break;
+					case '*' : S.push(opr1 * opr2); break;
+					case '/' : S.push(opr1 / opr2); break;
+				} // switch ³¡.
+			}
+		}
+		return S.pop();
+	}
+}
+
 public class _004_Ex7_4_295Page {
 	
 	public static void main(String[] args) {
