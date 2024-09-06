efd7c87a4537ef3c367db61fbda42c7616a714fc
dongso <ehdth5806@naver.com>
2024-09-07 01:01:39 +0900

    자바의자료구조 107번째 Commit.

--- src/Chapter07/_004_Ex7_4_295Page.java
+++ src/Chapter07/_004_Ex7_4_295Page.java
@@ -62,6 +62,11 @@ class OptExp2 {
 public class _004_Ex7_4_295Page {
 	
 	public static void main(String[] args) {
-	
+		OptExp2 opt = new OptExp2();
+		int result;
+		String exp = "35*62/-";
+		System.out.printf("\n후위표기식 : %s", exp);
+		result = opt.evalPostfix(exp);
+		System.out.printf("\n 연산결과 = %d \n", result);
 	}
 }
