6074eb760a16defd82c80f7889581191cb618397
dongso <ehdth5806@naver.com>
2024-10-07 22:52:02 +0900

    자바의자료구조 123번째 Commit.

--- src/Chapter08/_002_Ex8_2_322Page.java
+++ src/Chapter08/_002_Ex8_2_322Page.java
@@ -15,39 +15,70 @@ class ArrayCQueue implements Queue {
 	private char itemArray[];
 	
 	public ArrayCQueue(int queueSize) {
-		
+		front = 0;
+		rear = 0;
+		this.queueSize = queueSize;
+		itemArray = new char[this.queueSize];
 	}
 	
 	public boolean isEmpty() {
-		
-		return false;
+		return (front == rear);
 	}
 	
 	public boolean isFull() {
-		return true;
+		return (((rear + 1) % this.queueSize) == front);
 	}
 	
 	public void enQueue(char item) {
-	
+		if(isFull()) {
+			System.out.println("Inserting fail! Array Circular Queue is full!!");
+		} else {
+			rear = (rear + 1) % this.queueSize;
+			itemArray[rear] = item;
+			System.out.println("Inserted Item : " + item);
+		}
 	}
-
+	
 	public char deQueue() {
-		
-		return 0;
+		if(isEmpty()) {
+			System.out.println("Deleteing fail! Array Circular Queue is empty!!");
+			return 0;
+		} else {
+			front = (front + 1) % this.queueSize;
+			return itemArray[front];
+		}
 	}
 
 
 	public void delete() {
-		
+		if(isEmpty()) {
+			System.out.println("Deleting fail! Array Circular Queue is empty!!");
+		} else {
+			front = (front + 1) % this.queueSize;
+		}
 	}
 
 	public char peek() {
-	
-		return 0;
+		if(isEmpty()) {
+			System.out.println("Peeking fail! Array Circular Queue is empty!!");
+			return 0;
+		} else {
+			return itemArray[(front + 1) % this.queueSize];
+		}
 	}
 	
 	public void printQueue() {
-		
+		if(isEmpty()) {
+			System.out.println("Array Circular Queue is empty!!");
+		} else {
+			System.out.printf("Array Circular Queue>> ");
+			for(int i = (front + 1) % this.queueSize; i != (rear + 1) % this.queueSize; i = ++i % this.queueSize) {
+				System.out.printf("%c ", itemArray[i]);
+			} // for¹® ³¡.
+			
+			System.out.println();
+			System.out.println();
+		}
 	}
 }
 
