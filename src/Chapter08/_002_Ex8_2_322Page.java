ae23ac6523abc71e51b28d96c20009748dbf3e35
dongso <ehdth5806@naver.com>
2024-10-10 22:19:52 +0900

    자바의자료구조 124번째 Commit.

--- src/Chapter08/_002_Ex8_2_322Page.java
+++ src/Chapter08/_002_Ex8_2_322Page.java
@@ -86,6 +86,29 @@ class ArrayCQueue implements Queue {
 public class _002_Ex8_2_322Page {
 	
 	public static void main(String[] args) {
+		int queueSize = 4;
+		char deletedItem;
+		ArrayCQueue cQ = new  ArrayCQueue(queueSize);
 		
+		cQ.enQueue('A');
+		cQ.printQueue();
+		
+		cQ.enQueue('B');
+		cQ.printQueue();
+		
+		deletedItem = cQ.deQueue();
+		if(deletedItem != 0) {
+			System.out.println("deleted Item : " + deletedItem);
+		}
+		cQ.printQueue();
+		
+		cQ.enQueue('C');
+		cQ.printQueue();
+		
+		cQ.enQueue('D');
+		cQ.printQueue();
+		
+		cQ.enQueue('E');
+		cQ.printQueue();
 	}
 }
