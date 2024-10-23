a5805859720f1e30f95bafb76244d26fc7ef1342
dongso <ehdth5806@naver.com>
2024-10-23 23:01:12 +0900

    자바의자료구조 131번째 Commit.

--- src/Chapter08/_003_Ex8_3_331Page.java
+++ src/Chapter08/_003_Ex8_3_331Page.java
@@ -1,7 +1,49 @@
 package Chapter08;
 
 interface Queue1 {
-	// ==> 여기서 부터
+	boolean isEmpty();
+	void enQueue(char item);
+	char deQueue();
+	void delete();
+	char peek();
+}
+
+class QNode {
+	char data;
+	QNode link;
+}
+
+class LinkedQueue implements Queue1 {
+	QNode front;
+	QNode rear;
+	
+	public LinkedQueue() {
+		// ==> 여기서 부터
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+
+	public void enQueue(char item) {
+
+	}
+
+	public char deQueue() {
+		return 0;
+	}
+
+	public void delete() {
+
+	}
+
+	public char peek() {
+		return 0;
+	}
+	
+	public void printQueue() {
+		
+	}
 }
 
 public class _003_Ex8_3_331Page {
