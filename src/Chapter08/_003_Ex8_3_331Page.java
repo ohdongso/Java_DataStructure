34f363ed2bafe0e9b7c280279a4b51432abb2b7d
dongso <ehdth5806@naver.com>
2024-10-28 21:15:06 +0900

    자바의자료구조 132번째 Commit

--- src/Chapter08/_003_Ex8_3_331Page.java
+++ src/Chapter08/_003_Ex8_3_331Page.java
@@ -18,31 +18,76 @@ class LinkedQueue implements Queue1 {
 	QNode rear;
 	
 	public LinkedQueue() {
-		// ==> 여기서 부터
+		front = null;
+		rear = null;
 	}
 	
 	public boolean isEmpty() {
-		return false;
+		return (front == null);
 	}
 
 	public void enQueue(char item) {
-
+		QNode newNode = new QNode();
+		newNode.data = item;
+		if(isEmpty()) {
+			front = newNode;
+			rear = newNode;
+		} else {
+			rear.link = newNode;
+			rear = newNode;
+		}
+		
+		System.out.println("Inserted Item : " + item);
 	}
 
 	public char deQueue() {
-		return 0;
+		if(isEmpty()) {
+			System.out.println("Deleting fail! Linked Queue is empty!!");
+			return 0;
+		} else {
+			char item = front.data;
+			front = front.link;
+			if(front == null) {
+				rear = null;
+			}
+			
+			return item;
+		} // else 끝.
 	}
 
 	public void delete() {
-
+		if(isEmpty()) {
+			System.out.println("Deleting fail! Linked Queue is empty!!");
+		} else {
+			front = front.link;
+			if(front == null) {
+				rear = null;
+			}
+		}
 	}
 
 	public char peek() {
-		return 0;
+		if(isEmpty()) {
+			System.out.println("Peeking fail! Linked Queue is empty!!");
+			return 0;
+		} else {
+			return front.data;
+		}
 	}
 	
 	public void printQueue() {
-		
+		if(isEmpty()) {
+			System.out.printf("Linked Queue is empty!! %n %n");
+		} else {
+			QNode temp = front;
+			System.out.printf("Linked Queue>> ");
+			
+			while(temp != null) {
+				System.out.printf("%c ", temp.data);
+				temp = temp.link;
+			} // while문 끝.
+			System.out.println();
+		}
 	}
 }
 
