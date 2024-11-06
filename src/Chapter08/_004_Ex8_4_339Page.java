1b6e2825f9f3f7a29c32f21c582732a18668c71c
dongso <ehdth5806@naver.com>
2024-11-06 22:13:39 +0900

    자바의자료구조 138번째 Commit

--- src/Chapter08/_004_Ex8_4_339Page.java
+++ src/Chapter08/_004_Ex8_4_339Page.java
@@ -20,39 +20,131 @@ class DQueue {
 	}
 	
 	public void insertFront(char item) {
-		
+		DQNode newNode = new DQNode();
+		newNode.data = item;
+		if(isEmpty()) {
+			front = newNode;
+			rear = newNode;
+			newNode.rlink = null;
+			newNode.llink = null;
+		} else {
+			front.llink = newNode;
+			newNode.rlink = front;
+			newNode.llink = null;
+			front = newNode;
+		}
+		System.out.println("Front Inserted Item : " + item);
 	}
 	
 	public void insertRear(char item) {
-		
+		DQNode newNode = new DQNode();
+		newNode.data = item;
+		if(isEmpty()) {
+			front = newNode;
+			rear = newNode;
+			newNode.rlink = null;
+			newNode.llink = null;
+		} else {
+			rear.rlink = newNode;
+			newNode.rlink = null;
+			newNode.llink = rear;
+			rear = newNode;
+		}
+		System.out.println("Rear Inserted Item : " + item);
 	}
 	
 	public char deleteFront() {
-		return '0';
+		if(isEmpty()) {
+			System.out.println("Front Deleting fail! DQueue is empty!!");
+			return 0;
+		} else {
+			char item = front.data;
+			if(front.rlink == null) {
+				front = null;
+				rear = null;
+			} else {
+				front = front.rlink;
+				front.llink = null;
+			}
+			return item;
+		}
 	}
 	
 	public char deleteRear() {
-		return '0';
+		if(isEmpty()) {
+			System.out.println("Rear Deleting fail! DQueue is empty!!");
+			return 0;
+		} else {
+			char item = rear.data;
+			if(rear.llink == null) {
+				rear = null;
+				front = null;
+			} else {
+				rear = rear.llink;
+				rear.rlink = null;
+			}
+			return item;
+		}
 	}
 	
 	public void removeFront() {
-		
+		if(isEmpty()) {
+			System.out.println("Front Removing fail! DQueue is empty!!");
+		} else {
+			if(front.rlink == null) {
+				front = null;
+				rear = null;
+			} else {
+				front = front.rlink;
+				front.llink = null;
+			}
+		}
 	}
 	
 	public void removeRear() {
-		
+		if(isEmpty()) {
+			System.out.println("Rear Removing fail! DQueue is empty!!");
+		} else {
+			if(rear.llink == null) {
+				rear = null;
+				front = null;
+			} else {
+				rear = rear.llink;
+				rear.rlink = null;
+			}
+		}
 	}
 	
 	public char peekFront() {
-		return '0';
+		if(isEmpty()) {
+			System.out.println("Front Peeking fail! DQueue is empty!!");
+			return 0;
+		} else {
+			return front.data;
+		}
 	}
 	
 	public char peekRear() {
-		return '0';
+		if(isEmpty()) {
+			System.out.println("Rear Peeking fail! DQueue is empty!!");
+			return 0;
+		} else {
+			return rear.data;
+		}
 	}
 	
 	public void printDQueue() {
-		
+		if(isEmpty()) {
+			System.out.printf("DQueue is empty!! %n %n");
+		} else {
+			DQNode temp = front;
+			System.out.printf("DQueue>> ");
+			while(temp != null) {
+				System.out.printf("%c ", temp.data);
+				temp = temp.rlink;
+			} // while¹® ³¡.
+			System.out.println(); System.out.println();
+		}
 	}
 }
 
