775850b7174de17dc53598cbb18e9b238deb5940
dongso <ehdth5806@naver.com>
2025-01-22 23:26:53 +0900

    자바의자료구조 171번째 Commit

--- src/Chapter09/_001_Ex9_1_370Page.java
+++ src/Chapter09/_001_Ex9_1_370Page.java
@@ -47,6 +47,16 @@ public class _001_Ex9_1_370Page {
 		LinkedTree T = new LinkedTree();
 		
 		TreeNode n7 = T.makeBT(null, 'D', null);
+		TreeNode n6 = T.makeBT(null, 'C', null);
+		TreeNode n5 = T.makeBT(null, 'B', null);
+		TreeNode n4 = T.makeBT(null, 'A', null);
+		TreeNode n3 = T.makeBT(n6, '/', n7);
+		TreeNode n2 = T.makeBT(n4, '*', n5);
+		TreeNode n1 = T.makeBT(n2, '-', n3);
+		
+		System.out.printf("\n Preorder : ");
+		T.preorder(n1);
+		
 		
 	}
 }
