5cc24849298f5c0b0bd349374ce9bbc9c2799294
dongso <ehdth5806@naver.com>
2025-03-01 23:06:31 +0900

    자바의자료구조 188번째 Commit

--- src/Chapter09/_002_Ex9_2_383Page.java
+++ src/Chapter09/_002_Ex9_2_383Page.java
@@ -1,8 +1,71 @@
 package Chapter09;
 
+class TreeNode1 {
+	char data;
+	TreeNode1 left;
+	TreeNode1 right;
+}
+
+class BinarySearchTree {
+	private TreeNode1 root = new TreeNode1();
+	
+	public TreeNode1 insertKey(TreeNode1 root, char x) {
+		TreeNode1 p = root;
+		TreeNode1 newNode = new TreeNode1();
+		newNode.data = x;
+		newNode.left = null;
+		newNode.right = null;
+		if(p == null) {
+			return newNode;
+		} else if(newNode.data < p.data) {
+			p.left = insertKey(p.left, x);
+			return p;
+		} else if(newNode.data > p.data) {
+			p.right = insertKey(p.right, x);
+			return p;
+		} else {
+			return p;
+		}
+	}
+	
+	public void insertBST(char x) {
+		root = insertKey(root, x);
+	}
+	
+	public TreeNode1 searchBST(char x) {
+		TreeNode1 p = root;
+		while(p != null) {
+			if(x < p.data) {
+				p = p.left;
+			} else if(x > p.data) {
+				p = p.right;
+			} else {
+				return p;
+			}
+		}
+		return p;
+	}
+	
+	public void inorder (TreeNode1 root) {
+		if(root != null) {
+			inorder(root.left);
+			System.out.printf(" %C", root.data);
+			inorder(root.right);
+		}
+	}
+	
+	public void printBST() {
+		inorder(root);
+		System.out.println();
+	}
+	
+}
+
 public class _002_Ex9_2_383Page {
 	
 	public static void main(String[] args) {
+		// 여기서 부터
+		
 		
 	}	
 }
