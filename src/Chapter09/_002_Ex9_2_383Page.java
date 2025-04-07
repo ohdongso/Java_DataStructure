b4165894e4e636f6e54b446ff6cae962761a6178
dongso <ehdth5806@naver.com>
2025-04-07 23:16:32 +0900

    자바의자료구조 203번째 Commit

--- src/Chapter09/_002_Ex9_2_383Page.java
+++ src/Chapter09/_002_Ex9_2_383Page.java
@@ -71,8 +71,8 @@ public class _002_Ex9_2_383Page {
 	public static void main(String[] args) {
 		BinarySearchTree bsT = new BinarySearchTree();
 		bsT.insertBST('G');
-		bsT.insertBST('I');
-		bsT.insertBST('H');
+		// bsT.insertBST('I');
+		// bsT.insertBST('H');
 		
 		// bsT.insertBST('D');
 		
